9ee16ecb09b98905bbd853375c935d04bcb9a974
Dmytro Voronov <dimolg22@gmail.com>
2022-10-14 16:37:28 +0300

    complete forgot password functionality

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,12 +1,25 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import {Link} from "react-router-dom";
 import OAuth from "../components/OAuth";
+import {toast} from "react-toastify";
+import {getAuth, sendPasswordResetEmail} from "firebase/auth";
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
+    }
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+        try {
+          const auth = getAuth()
+           await sendPasswordResetEmail(auth, email)
+            toast.success('Success')
+
+        }catch (e) {
+            toast.error('Could not send reset password')
+        }
     }
     return (
         <section>
@@ -16,7 +29,7 @@ const ForgotPassword = () => {
                     <img className={'w-full rounded-2xl'} src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80" alt="key"/>
                 </div>
                 <div  className={'w-full md:w-[67%] lg:w-[40%] lg:ml-20'}>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <input
                             className={'mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'}
                             type="email"
@@ -31,12 +44,13 @@ const ForgotPassword = () => {
                             </p>
                             <p><Link className={'text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'} to={'/sign-in'}>Sign in instead</Link></p>
                         </div>
+                        <button className={'w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'} type={'submit'}>Send reset password</button>
+                        <div className={'my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'}>
+                            <p className={'text-center font-semibold mx-4'}>OR</p>
+                        </div>
+                        <OAuth/>
                     </form>
-                    <button className={'w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'} type={'submit'}>Send reset password</button>
-                    <div className={'my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'}>
-                        <p className={'text-center font-semibold mx-4'}>OR</p>
-                    </div>
-                    <OAuth/>
+
                 </div>
             </div>
         </section>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,12 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import {AiFillEye, AiFillEyeInvisible} from "react-icons/ai";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import OAuth from "../components/OAuth";
+import {signInWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {toast} from "react-toastify";
 
 const SignIn = () => {
+    const navigate = useNavigate()
     const [showPassword, setShowPassword] = useState(false)
     const [formData, setFormData] = useState({
         email: '',
@@ -16,7 +19,19 @@ const SignIn = () => {
             [e.target.id]: e.target.value
         }))
     }
-    console.log(formData)
+    const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+        try {
+          const auth = getAuth()
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+            if (userCredentials.user){
+                navigate('/')
+            }
+
+        }catch (e) {
+            toast.error('Bad user credentials')
+        }
+    }
     return (
         <section>
            <h1 className={'text-3xl text-center mt-6 font-bold'}>Sign In</h1>
@@ -25,7 +40,7 @@ const SignIn = () => {
                     <img className={'w-full rounded-2xl'} src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80" alt="key"/>
                 </div>
                 <div  className={'w-full md:w-[67%] lg:w-[40%] lg:ml-20'}>
-                    <form>
+                    <form onSubmit={handleOnSubmit}>
                         <div className={'relative mb-6'}>
                         <input
                             className={'mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'}
@@ -51,12 +66,13 @@ const SignIn = () => {
                             </p>
                             <p><Link className={'text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'} to={'/forgot-password'}>Forgot password?</Link></p>
                         </div>
-                    </form>
                         <button className={'w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'} type={'submit'}>Sign in</button>
-                    <div className={'my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'}>
-                        <p className={'text-center font-semibold mx-4'}>OR</p>
-                    </div>
+                        <div className={'my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'}>
+                            <p className={'text-center font-semibold mx-4'}>OR</p>
+                        </div>
                         <OAuth/>
+                    </form>
+
                 </div>
             </div>
         </section>
