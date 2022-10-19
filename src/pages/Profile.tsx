b78e05bb9b1d0b19c4efadd71e74d062424f51ab
Dmytro Voronov <dimolg22@gmail.com>
2022-10-19 20:17:20 +0300

    add UI create listing

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
-import React, {ChangeEvent, FormEvent, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {getAuth, updateProfile} from "firebase/auth";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {doc, updateDoc} from "firebase/firestore";
 import {db} from "../firebase";
+import {FcHome} from "react-icons/fc";
+import {Link} from 'react-router-dom'
 
 const Profile = () =>{
     const navigate = useNavigate()
@@ -65,6 +67,12 @@ const Profile = () =>{
                        </div>
 
                    </form>
+                   <button className={'w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700'} type={'submit'}>
+                       <Link className={'flex justify-center items-center'} to={'/create-listing'}>
+                           <FcHome className={'mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'}/> Sell or rent your home
+                       </Link>
+
+                   </button>
                </div>
            </section>
         </>
